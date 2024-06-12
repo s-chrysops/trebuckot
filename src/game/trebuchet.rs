69e37df63721575d70aeac4b6df95b557b654982
stenotomus-chrysops <joshuaDA6127@gmail.com>
2024-06-12 00:30:37 -0700

    refactor: replace custom type for matricies

--- src/game/trebuchet.rs
+++ src/game/trebuchet.rs
@@ -1,5 +1,5 @@
 use core::f32::consts;
-use macroquad::math::{I64Vec2, Vec2, Vec3, vec2, vec3};
+use macroquad::math::*;
 use crate::utils::*;
 
 const GRAVITY: f32 = 9.81;
@@ -199,18 +199,22 @@ impl Trebuchet {
     }
 
     pub fn run(&mut self, dt: f32) {
-        let stage: Box<dyn Fn(f32, Vec3, Vec3) -> Vec3x2> = match self.state {
+        let mat = mat3a(
+            vec3a(self.arm.angle, self.weight.angle, self.sling.angle),
+            vec3a(self.arm.velocity, self.weight.velocity, self.sling.velocity),
+            Vec3A::ZERO
+        );
 
+        let stage: Box<dyn Fn(f32, Mat3A) -> Mat3A> = match self.state {
             TrebuchetState::Stage1 => {
-                let Vec3x2{q: _, w: Vec3{x: aw_prime, ..}} = self.stage_1 ( 
+                let Mat3A{x_axis: _, y_axis: Vec3A{x: aw_prime, ..}, ..} = self.stage_1 ( 
                     dt,
-                    vec3(self.arm.angle, self.weight.angle, self.sling.angle),
-                    vec3(self.arm.velocity, self.weight.velocity, self.sling.velocity)
+                    mat,
                 );
                 if self.ground_force(aw_prime) <= 0.0 {
                     self.state = TrebuchetState::Stage2;
                 }
-                Box::new(|dt: f32, x: Vec3, y: Vec3| self.stage_1(dt, x, y))
+                Box::new(|dt: f32, x: Mat3A| self.stage_1(dt, x))
             }
 
             TrebuchetState::Stage2 => {
@@ -218,22 +222,17 @@ impl Trebuchet {
                     self.m_proj = 0.01;
                     self.state = TrebuchetState::Stage3;
                 }
-                Box::new(|dt: f32, x: Vec3, y: Vec3| self.stage_2(dt, x, y))
+                Box::new(|dt: f32, x: Mat3A| self.stage_2(dt, x))
             }
 
             TrebuchetState::Stage3 => {
-                Box::new(|dt: f32, x: Vec3, y: Vec3| self.stage_2(dt, x, y))
+                Box::new(|dt: f32, x: Mat3A| self.stage_2(dt, x))
             }
         };
         
-        let rk4_results = rk4 (
-            vec3(self.arm.angle, self.weight.angle, self.sling.angle),
-            vec3(self.arm.velocity, self.weight.velocity, self.sling.velocity),
-            dt, 
-            stage
-        );
-        Vec3 {x: self.arm.angle, y: self.weight.angle, z: self.sling.angle}         = rk4_results.q;
-        Vec3 {x: self.arm.velocity, y: self.weight.velocity, z:self.sling.velocity} = rk4_results.w;
+        let rk4_results = rk4(mat, dt, stage);
+        Vec3A {x: self.arm.angle, y: self.weight.angle, z: self.sling.angle}         = rk4_results.x_axis;
+        Vec3A {x: self.arm.velocity, y: self.weight.velocity, z:self.sling.velocity} = rk4_results.y_axis;
     }
 
     fn ground_force(&self, aw_prime: f32) -> f32 {
@@ -248,7 +247,7 @@ impl Trebuchet {
             / (self.arm.angle + self.sling.angle).sin())
     }
 
-    fn stage_1(&self, dt: f32, angles: Vec3, velocities: Vec3) -> Vec3x2 {
+    fn stage_1(&self, dt: f32, mat: Mat3A) -> Mat3A {
         let lal = self.arm.long_length;
         let las = self.arm.short_length;
         let cga = self.arm.center;
@@ -262,49 +261,53 @@ impl Trebuchet {
         let ia = self.arm.inertia;
         let iw = self.weight.inertia;
 
-        let aq = angles.x;
-        let wq = angles.y;
-        let sq = angles.z;
+        let aq = mat.x_axis.x;
+        let wq = mat.x_axis.y;
+        let sq = mat.x_axis.z;
 
-        let aw = velocities.x;
-        let ww = velocities.y;
-        let sw = velocities.z;
-
-        #[rustfmt::skip]
-        let m11 = -mp * lal.powi(2)
-            * (-1.0 + 2.0 * aq.sin() * sq.cos() / (aq + sq).sin()) + ia + iw + ma * cga.powi(2) 
-            + mp * lal.powi(2) * aq.sin().powi(2) / (aq + sq).sin().powi(2) + mw * (las.powi(2) 
-            + lw.powi(2)+ 2.0 * las * lw * wq.cos());
-        let m12 = iw + lw * mw * (lw + las * wq.cos());
-        let m21 = iw + lw * mw * (lw + las * wq.cos());
-        let m22 = iw + mw * lw.powi(2);
+        let aw = mat.y_axis.x;
+        let ww = mat.y_axis.y;
+        let sw = mat.y_axis.z;
 
         #[rustfmt::skip]
-        let r1 = GRAVITY * cga * ma * aq.sin() + lal * ls * mp * (sq.sin() * (aw + sw).powi(2) 
+        let m = Mat2::from_cols_array(&[
+            -mp * lal.powi(2) * (-1.0 + 2.0 * aq.sin() * sq.cos() / (aq + sq).sin()) + ia + iw + ma 
+            * cga.powi(2) + mp * lal.powi(2) * aq.sin().powi(2) / (aq + sq).sin().powi(2) + mw 
+            * (las.powi(2) + lw.powi(2)+ 2.0 * las * lw * wq.cos()),
+            iw + lw * mw * (lw + las * wq.cos()),
+            iw + lw * mw * (lw + las * wq.cos()),
+            iw + mw * lw.powi(2),
+        ]);
+
+        #[rustfmt::skip]
+        let r = vec2(
+            GRAVITY * cga * ma * aq.sin() + lal * ls * mp * (sq.sin() * (aw + sw).powi(2) 
             + sq.cos() * ((aq + sq).cos() * sw * (sw + 2.0 * aw) / (aq + sq).sin() 
             + ((aq + sq).cos() / (aq + sq).sin() + lal * aq.cos() / (ls * (aq + sq).sin())) 
             * aw.powi(2))) + lal * mp * aq.sin() * (lal * sq.sin() * aw.powi(2) - ls
             * ((aq + sq).cos() * sw * (sw + 2.0 * aw) / (aq  + sq).sin() + ((aq + sq).cos()
             / (aq + sq).sin() + lal * aq.cos() / (ls * (aq + sq).sin())) * aw.powi(2))) 
-            / (aq + sq).sin() - GRAVITY * mw * (las * aq.sin() + lw * (aq + wq).sin()) - las * lw 
-            * mw * wq.sin() * (aw.powi(2) - (aw + ww).powi(2));
-        let r2 = -lw * mw * (GRAVITY * (aq + wq) + las * wq.sin() * aw.powi(2));
+            / (aq + sq).sin() - GRAVITY * mw * (las * aq.sin() + lw * (aq + wq).sin()) - las 
+            * lw * mw * wq.sin() * (aw.powi(2) - (aw + ww).powi(2)),
+            -lw * mw * (GRAVITY * (aq + wq) + las * wq.sin() * aw.powi(2))
+        );
 
-        let aw_prime = (r1 * m22 - r2 * m12) / (m11 * m22 - m12 * m21);
-        let ww_prime = -(r1 * m21 - r2 * m11) / (m11 * m22 - m12 * m21);
+        let arm_weight_accel = m.inverse().mul_vec2(r);
         #[rustfmt::skip]
         let sw_prime = -(aq + sq).cos() * sw * (sw + 2.0 * aw) / (aq + sq).sin() 
             - ((aq + sq).cos() / (aq + sq).sin() + lal * aq.cos() / (ls * (aq + sq).sin())) 
-            * aw.powi(2) - (lal * aq.sin() + ls * (aq + sq).sin()) * aw_prime 
+            * aw.powi(2) - (lal * aq.sin() + ls * (aq + sq).sin()) * arm_weight_accel.x 
             / (ls * (aq + sq).sin());
+        let accelerations: Vec3A = arm_weight_accel.extend(sw_prime).into();
                 
-        Vec3x2 {
-            q: vec3(aw + aw_prime * dt, ww + ww_prime * dt, sw + sw_prime * dt),
-            w: vec3(aw_prime, ww_prime, sw_prime)
-        }
+        mat3a(
+            mat.y_axis + accelerations * dt,
+            accelerations,
+            Vec3A::ZERO,
+        )
     }
 
-    fn stage_2(&self, dt: f32, angles: Vec3, velocities: Vec3) -> Vec3x2 {
+    fn stage_2(&self, dt: f32, mat: Mat3A) -> Mat3A {
         let lal = self.arm.long_length;
         let las = self.arm.short_length;
         let cga = self.arm.center;
@@ -318,82 +321,79 @@ impl Trebuchet {
         let ia = self.arm.inertia;
         let iw = self.weight.inertia;
 
-        let aq = angles.x;
-        let wq = angles.y;
-        let sq = angles.z;
+        let aq = mat.x_axis.x;
+        let wq = mat.x_axis.y;
+        let sq = mat.x_axis.z;
 
-        let aw = velocities.x;
-        let ww = velocities.y;
-        let sw = velocities.z;
-
-        #[rustfmt::skip]
-        let m11 = ia + iw + ma * cga.powi(2) + mp 
-            * (lal.powi(2) + ls.powi(2) + 2.0 * lal * ls * sq.cos()) 
-            + mw * (las.powi(2) + lw.powi(2) + 2.0 * las * lw * wq.cos());
-        let m12 = iw + lw * mw * (lw + las * wq.cos());
-        let m13 = ls * mp * (ls + lal * sq.cos());
-        let m21 = iw + lw * mw * (lw + las * wq.cos());
-        let m22 = iw + mw * lw.powi(2);
-        let m31 = ls * mp * (ls + lal * sq.cos());
-        let m33 = mp * ls.powi(2);
+        let aw = mat.y_axis.x;
+        let ww = mat.y_axis.y;
+        let sw = mat.y_axis.z;
 
         #[rustfmt::skip]
-        let r1 = GRAVITY * cga * ma * aq.sin() + GRAVITY * mp 
-            * (lal * aq.sin() + ls * (aq + sq).sin()) - GRAVITY * mw 
-            * (las * aq.sin() + lw * (aq + wq).sin()) - lal * ls * mp * sq.sin() 
-            * (aw.powi(2) - (aw + sw).powi(2)) 
-            - las * lw * mw * wq.sin() 
-            * (aw.powi(2) - (aw + ww).powi(2));
-        let r2 = -lw * mw * (GRAVITY * (aq + wq).sin() + las * wq.sin() * aw.powi(2));
-        let r3 = ls * mp * (GRAVITY * (aq + sq).sin() - lal * sq.sin() * aw.powi(2));
+        let m = Mat3A::from_cols_array(&[
+            ia + iw + ma * cga.powi(2) + mp * (lal.powi(2) + ls.powi(2) + 2.0 * lal * ls 
+            * sq.cos()) + mw * (las.powi(2) + lw.powi(2) + 2.0 * las * lw * wq.cos()),
+            iw + lw * mw * (lw + las * wq.cos()),
+            ls * mp * (ls + lal * sq.cos()),
+            iw + lw * mw * (lw + las * wq.cos()),
+            iw + mw * lw.powi(2),
+            0.0,
+            ls * mp * (ls + lal * sq.cos()),
+            0.0,
+            mp * ls.powi(2),
+        ]);
 
-        let aw_prime = -(r1 * m22 * m33 - r2 * m12 * m33 - r3 * m13 * m22) 
-            / (m13 * m22 * m31 - m33 * (m11 * m22 - m12 * m21));
-        let ww_prime = (r1 * m21 * m33 - r2 * (m11 * m33 - m13 * m31) - r3 * m13 * m21) 
-            / (m13 * m22 * m31 - m33 * (m11 * m22 - m12 * m21));
-        let sw_prime = (r1 * m22 * m31 - r2 * m12 * m31 - r3 * (m11 * m22 - m12 * m21)) 
-            / (m13 * m22 * m31 - m33 * (m11 * m22 - m12 * m21));
+        #[rustfmt::skip]
+        let r = vec3a(
+            GRAVITY * cga * ma * aq.sin() + GRAVITY * mp * (lal * aq.sin() + ls * (aq + sq).sin()) 
+            - GRAVITY * mw * (las * aq.sin() + lw * (aq + wq).sin()) - lal * ls * mp * sq.sin() 
+            * (aw.powi(2) - (aw + sw).powi(2)) - las * lw * mw * wq.sin() * (aw.powi(2) 
+            - (aw + ww).powi(2)),
+            -lw * mw * (GRAVITY * (aq + wq).sin() + las * wq.sin() * aw.powi(2)),
+            ls * mp * (GRAVITY * (aq + sq).sin() - lal * sq.sin() * aw.powi(2)),
+        );
+
+        let accelerations = m.inverse().mul_vec3a(r);
         
-        Vec3x2 {
-            q: vec3(aw + aw_prime * dt, ww + ww_prime * dt, sw + sw_prime * dt),
-            w: vec3(aw_prime, ww_prime, sw_prime)
-        }
+        mat3a(
+            mat.y_axis + accelerations * dt,
+            accelerations,
+            Vec3A::ZERO
+        )
     }
 }
 
-// Container for set of angular positions and velocities or their derivatives that can be multiplied
-struct Vec3x2{
-    q: Vec3,
-    w: Vec3,
-}
-impl std::ops::Mul<Vec3x2> for f32 {
-    type Output = Vec3x2;
-    fn mul(self, rhs: Vec3x2) -> Vec3x2 {
-        Vec3x2 {
-            q: Vec3 {
-                x: self.mul(rhs.q.x),
-                y: self.mul(rhs.q.y),
-                z: self.mul(rhs.q.z),
-            },
-            w: Vec3 {
-                x: self.mul(rhs.w.x),
-                y: self.mul(rhs.w.y),
-                z: self.mul(rhs.w.z),
-            }
-        }
-    }
-}
-
-fn rk4<T>(x: Vec3, y: Vec3, dt: f32, f: T) -> Vec3x2
+use std::ops::{Add, Mul, Div};
+fn rk4<T, U>(x: T, dt: f32, f: U) -> T
 where
-    T: Fn(f32, Vec3, Vec3) -> Vec3x2
+    T: Copy + Add<Output = T> + Mul<f32, Output = T> + Div<f32, Output = T>,
+    f32: Mul<T, Output = T>,
+    U: Fn(f32, T) -> T,
 {
-    let k1 = dt * f(dt, x, y);
-    let k2 = dt * f(dt, x + k1.q * 0.5, y + k1.w * 0.5);
-    let k3 = dt * f(dt, x + k1.q * 0.5, y + k2.w * 0.5);
-    let k4 = dt * f(dt, x + k3.q, y + k3.w);
-    Vec3x2{   
-        q: (x + (k1.q + 2.0 * k2.q + 2.0 * k3.q + k4.q) / 6.0),
-        w: (y + (k1.w + 2.0 * k2.w + 2.0 * k3.w + k4.w) / 6.0),
+    let k1 = dt * f(dt, x);
+    let k2 = dt * f(dt, x + 0.5 * k1);
+    let k3 = dt * f(dt, x + 0.5 * k1);
+    let k4 = dt * f(dt, x + k3);
+    x + (k1 + k2 * 2.0  + k3 * 2.0 + k4).div(6.0) 
+}
+
+#[cfg(test)]
+mod test {
+    use macroquad::math::Mat3;
+
+    #[test]
+    fn mat_mul(){
+        let col = [
+            1.0, 1.0, 1.0,
+            2.0, 2.0, 2.0,
+            0.0, 0.0, 0.0,
+        ];
+        let add = [
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            0.0, 0.0, 0.0,
+        ];
+        let m = Mat3::from_cols_array(&col);
+        println!("{:?}", m * 2.0 + Mat3::from_cols_array(&add))
     }
 }
